5082976f33a2dd505c4946e49879787ef66ecf7c
Rodrigo Menezes <rodrusantu7@gmail.com>
2022-12-28 16:48:36 -0300

    Update estudos 12/2022

--- Workspace/js_fundamentos/decisaoIF.js
+++ Workspace/js_fundamentos/decisaoIF.js
@@ -1,9 +1,23 @@
+// * Com a extensão Node.js Exec pasta apertar a tecla Fu que ele executa
+
 
 let idade = 18
-
 if ( idade >= 18 ){
   console.log("Você possui idade mínima, pode adquirir habilitação")
 } else {
   console.log("Você ainda não possui idade mínima")
 }
 
+// ************************************
+
+var velocidade = 60.5
+console.log(`A velocidade do seu carro está a ${velocidade} km por hora`)
+
+// ************************************
+
+var vel = 90
+if (vel < 60) {
+  console.log(`A velocidade do seu carro está a ${vel} km por hora, você PODE SEGUIR`)
+} else {
+  console.log(`A velocidade do seu carro está a ${vel} km por hora, você SERÁ MULTADO`)
+}
--- Workspace/js_praticando/calculaIMC.js
+++ Workspace/js_praticando/calculaIMC.js
@@ -4,6 +4,6 @@ const prompt = promptSync()
 // CALCULA IMC
     let peso = prompt("Digite seu peso: ")
     let altura = prompt("Digite sua altura: ")
-    resultado = peso/altura**2
+    let resultado = peso / altura**2
 
     console.log(resultado.toFixed(2));
